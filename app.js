6e941d0015e857d4b1a3ad716b4a8736abf616e9
ize-send <adavize.hassan@send.ng>
2023-04-16 17:57:28 +0100

    add env

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
+require('dotenv').config();
 const app = express()
-const port = 3000
+const { PORT } = process.env
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
 })

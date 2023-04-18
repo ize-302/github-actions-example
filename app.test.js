20e26084bdb2b0c7c7f1d0311e813d3e9f0a785a
ize-send <adavize.hassan@send.ng>
2023-04-18 07:20:38 +0100

    update

--- app.test.js
+++ app.test.js
@@ -1,10 +1,21 @@
 const request = require("supertest");
-const app = require("./app");
+const app = require("./app.js");
+
+// describe("GET / ", () => {
+//   test('It should respond with message object', async () => {
+//     // const response = await request(app).get('/')
+//     // expect(response.body).toEqual({ message: 'Hello World!' });
+//     // expect(response.statusCode).toBe(200);
+//   })
+// });
+
 
 describe("GET / ", () => {
-  test('It should respond with message object', async () => {
-    const response = await request(app).get('/')
-    expect(response.body).toEqual({ message: 'Hello World!' });
-    expect(response.statusCode).toBe(200);
+  describe('It should respond with message object', () => {
+    test("should respond with a 200 status code", async () => {
+      const response = await request(app).get("/")
+      expect(response.statusCode).toBe(200);
+      expect(response.body).toEqual({ message: 'Hello World!' });
+    })
   })
 });

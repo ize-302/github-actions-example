ffae0963b63368a7da0386ba48fa18f4cd45620b
ize-send <adavize.hassan@send.ng>
2023-04-18 10:09:19 +0100

    fix response

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const app = express()
 
 app.get('/', (req, res) => {
-  res.status(200).send({ message: 'Hello World' })
+  res.status(200).send({ message: 'Hello World!' })
 })
 
 module.exports = app;

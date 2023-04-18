45a37a59a23f84f7e745578dba14461b96214d84
ize-send <adavize.hassan@send.ng>
2023-04-18 09:09:28 +0100

    fix test, refactor

--- app.test.js
+++ app.test.js
@@ -1,15 +1,6 @@
 const request = require("supertest");
 const app = require("./app.js");
 
-// describe("GET / ", () => {
-//   test('It should respond with message object', async () => {
-//     // const response = await request(app).get('/')
-//     // expect(response.body).toEqual({ message: 'Hello World!' });
-//     // expect(response.statusCode).toBe(200);
-//   })
-// });
-
-
 describe("GET / ", () => {
   describe('It should respond with message object', () => {
     test("should respond with a 200 status code", async () => {
